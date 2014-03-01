29747165e4c414c040b11fda9c392580e36484db
akashagarwal <akashslg@gmail.com>
2014-03-01 07:45:30 +0530

    IRC connections, event detection added

--- README.md
+++ README.md
@@ -2,29 +2,13 @@ Bounty-Web-Expenses
 ===================
 
 Tracking expenses accurately can be the biggest headache among a group of roommates or co-workers. There are many apps which do this kind of task.
+</br>
 
-Your task is to build a similar webapp using a client side Javascript MVC framework.
+<h1>Now you can track expenses while chatting on IRC channel #Artooxt on irc.freenode.net </h1>
 
-**Snaps for Reference - Basic Functionalities Required**
+Installation: npm install irc
+Run the bot : node bot.js
 
-Add New Member
+<h1> Message Syntax : Give [nick] [amount] <h1>
 
-![Add New Member](http://artoogithubdocs.s3.amazonaws.com/bounty/add-new-member.png)
 
-Add New Expense
-
-![Add New Expense](http://artoogithubdocs.s3.amazonaws.com/bounty/add-expense.png)
-
-Summary Sheet
-
-![Summary Sheet](http://artoogithubdocs.s3.amazonaws.com/bounty/summary.png) 
-
-**Brownie Points**
-
-1.  Data Visualization - Charts and graphs to show division of expenses
-2.  Options like Print, Export to xls etc 
-3.  Server Side Synchronization
-
-**Technologies we love**
-
-Ember.js, Backbone.js, D3.js, Node.js, Phantom.js, CouchDB
---
+++ bot.js
@@ -0,0 +1,121 @@
+var config = {
+		channels: ["#Artooxt"],
+	  	server: "irc.freenode.net",
+		botName: "Akash_xt_bot"
+};
+
+function cloneObject(source) {
+	    for (i in source) {
+		            if (typeof source[i] == 'source') {
+				                this[i] = new cloneObject(source[i]);
+						        }
+			            else{
+					                this[i] = source[i];
+								}
+				        }
+}
+
+
+var irc = require("irc");
+
+var expenses = new Object();
+expenses["akashagarwal"]=0;
+
+var bot = new irc.Client(config.server, config.botName, {
+			channels: config.channels
+			});
+
+bot.addListener("join", function(channel, who) {
+			// Welcome them in!
+			bot.say(channel, who + "...dude...welcome to the Artoo expenses chat room!");
+			expenses[who]=0;
+			});
+
+
+bot.addListener("message", function(from, to, text, message) {
+			
+			match=text.match("Give");
+			if (match!=null)
+			{
+				for (var x in expenses)
+				{
+			//		bot.say(config.channels[0],x);	
+					nickmatch=text.match(x);
+					if (nickmatch!=null)
+					{
+						var begin=text.lastIndexOf(" ")+1;
+
+						if (begin==0)
+						{
+							bot.say(config.channels[0],"Syntax Error");	
+							break;
+						}
+						var amount=Number(text.substr(begin));
+						if (isNaN(amount))
+						{
+							bot.say(config.channels[0],"Syntax Error");	
+							break;
+						}
+						expenses[x]+=amount;
+						expenses[from]-=amount;
+						var temp=new cloneObject(expenses);
+						var flag=0;
+						while(flag==0)
+						{
+							flag=1;
+							for (var x in temp)
+							{
+								if (temp[x]<0)
+								{
+									flag=0;
+									var a = Math.abs(temp[x])
+									for(var y in temp)
+									{
+										if (temp[y] >= a )
+										{
+											temp[x] += a;
+											temp[y] -= a;
+											bot.say(config.channels[0], x + "->" + y + " : " + a);
+											break;
+										}
+									}
+								/*
+									for(var y in temp)
+									{
+										if (temp[x] < 0 )
+										{
+											if (temp[y] > 0)
+											{
+												if (a< temp[y])
+												{
+													temp[x] += a;
+													temp[y] -= a;
+													break;
+													bot.say(config.channels[0], x + "->" + y + " : " + a);
+												}
+												else
+												{
+													temp[x] += temp[y];
+													temp[y] -= temp[y];
+													bot.say(config.channels[0], x + "->" + y + " : " + temp[y]);
+										//			break;
+												}
+											}
+										}
+										else
+											break;
+									}
+								*/	
+								
+								}
+	
+							}
+						}
+						break;
+
+					}
+				}
+			}
+		});
+
+
